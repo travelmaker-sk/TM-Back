7929df81026245d6e4e54a82a70af27afa1ea481
Bae-Ji-Won <qowhxk@naver.com>
2022-05-16 01:36:39 +0900

    Entity-DTO(장소)

---
+++ src/main/java/com/notice/board/entity/Place.java
@@ -0,0 +1,28 @@
+package com.notice.board.entity;
+
+
+import lombok.Data;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity    // DB에 있는 테이블을 의미한다라는 것
+@Data
+public class Place{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int placeid;
+
+    private String placelocation;
+    private String placename;
+    private String placedate;
+    private String placeweather;
+    private String placescore;
+    private String placememo;
+    private String placetag;
+    private String placefilename;
+    private String placefilepath;
+}
