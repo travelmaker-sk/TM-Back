1f139711e2ddfbf0307d8b67013095891f75f581
Bae-Ji-Won <qowhxk@naver.com>
2022-05-16 01:38:41 +0900

    Repository(맛집)

--- src/main/java/com/notice/board/repository/StoreBoardRepository.java
+++ src/main/java/com/notice/board/repository/StoreBoardRepository.java
@@ -1,11 +1,10 @@
 package com.notice.board.repository;
 
-//import spring.basic.demo.domain.Data;
 
 import com.notice.board.entity.Store;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface BoardRepository extends JpaRepository<Store,Object> {
+public interface StoreBoardRepository extends JpaRepository<Store,Object> {
 }
