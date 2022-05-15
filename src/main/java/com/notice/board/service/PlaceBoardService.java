1f57504fc0db6a933e39da46eca1053e4612752e
Bae-Ji-Won <qowhxk@naver.com>
2022-05-16 01:39:10 +0900

    Service(명소)

---
+++ src/main/java/com/notice/board/service/PlaceBoardService.java
@@ -0,0 +1,58 @@
+package com.notice.board.service;
+
+import com.notice.board.entity.Place;
+import com.notice.board.repository.PlaceBoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+import java.util.UUID;
+
+@Service     //spring bean 사용하여 따로 설정했으므로 삭제해야함
+public class PlaceBoardService {
+
+    @Autowired
+    private PlaceBoardRepository repository;
+
+    public PlaceBoardService(PlaceBoardRepository repository){
+        this.repository = repository;
+    }
+
+    public void placewrite(Place place, MultipartFile file) throws IOException {
+
+        String projectPath = System.getProperty("user.dir") + "\\src\\main\\resources\\static\\files\\place";        // 파일 경로 저장
+
+        UUID uuid = UUID.randomUUID();      // 식별자 , 파일 이름을 랜덤으로 저장하기 위해
+
+        String filename = uuid + "_" + file.getOriginalFilename();      // 파일 이름 변수에 랜덤이름 저장 => uuid(랜덤 이름)_업로드한파일명
+
+        File saveFile = new File(projectPath,filename);   // projectpath 경로에 name이름으로 저장할것
+
+        file.transferTo(saveFile);
+
+        place.setPlacefilename(filename);                   // 파일 이름  db 저장
+        place.setPlacefilepath("/files/place/"+filename);         // 파일 경로 db 저장
+
+        repository.save(place);
+    }
+
+    public void placeeditwrite(Place place){
+        repository.save(place);
+    }
+
+    public List<Place> placelist(){
+        return repository.findAll();
+    }
+
+    public Place placedetail(int id){
+        return repository.findById(id).get();
+    }
+
+    public void placedelete(int id){
+
+        repository.deleteById(id);
+    }
+}
